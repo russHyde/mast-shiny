e1be0dc3fa38ab7badca3fd423682c3c2e77408c
Russ Hyde <russ.hyde.data@gmail.com>
2021-01-31 15:36:35 +0000

    wire outputs to the selected dataset

--- datasets/app.R
+++ datasets/app.R
@@ -6,6 +6,16 @@ ui <- fluidPage(
   tableOutput("table")
 )
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  output$summary <- renderPrint({
+    dataset <- get(input[["dataset"]], "package:datasets")
+    summary(dataset)
+  })
+
+  output$table <- renderTable({
+    dataset <- get(input[["dataset"]], "package:datasets")
+    dataset
+  })
+}
 
 shinyApp(ui, server)
