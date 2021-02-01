8f5352d420f7247720534f36a1196c0562e329bc
Russ Hyde <russ.hyde.data@gmail.com>
2021-02-01 12:18:01 +0000

    extend multiplier: user defined y (Ch3/Ex3)

--- ex/ch3/multiplier/app.R
+++ ex/ch3/multiplier/app.R
@@ -4,13 +4,14 @@ library(shiny)
 
 ui <- fluidPage(
   sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
-  "then x times 5 is",
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 5),
+  "then, x times y is",
   textOutput("product")
 )
 
 server <- function(input, output, server) {
   output$product <- renderText({
-    input[["x"]] * 5
+    input[["x"]] * input[["y"]]
   })
 }
 
