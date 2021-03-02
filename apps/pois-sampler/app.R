7cf3691f762187233dd6f9050853cfb2af2c2ca3
Russ Hyde <russ.hyde.data@gmail.com>
2021-03-02 15:08:53 +0000

    sec 4.5: use eventReactive to remove direct dependency on some inputs (only compute x upon actionButton press)

--- apps/pois-sampler/app.R
+++ apps/pois-sampler/app.R
@@ -18,30 +18,30 @@ freqpoly <- function(x1, x2, binwidth = 0.1, xlim = c(-3, 3)) {
 
 ui <- fluidPage(
   fluidRow(
-    column(3,
+    column(
+      3,
       numericInput("lambda1", label = "lambda1", value = 3),
       numericInput("lambda2", label = "lambda2", value = 5),
-      numericInput("n", label = "n", value = 1e4, min = 0)
+      numericInput("n", label = "n", value = 1e4, min = 0),
+      actionButton("simulate", "Simulate!")
     ),
     column(9, plotOutput("hist"))
   )
 )
 
 server <- function(input, output, session) {
-  # samples are invalidated (& therefore reran) every 500ms
-  timer <- reactiveTimer(500)
-
-  x1 <- reactive({
-    timer()
+  x1 <- eventReactive(input$simulate, {
     rpois(input$n, input$lambda1)
   })
-  x2 <- reactive({
-    timer()
+  x2 <- eventReactive(input$simulate, {
     rpois(input$n, input$lambda2)
   })
-  output$hist <- renderPlot({
-    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
-  }, res = 96)
+  output$hist <- renderPlot(
+    {
+      freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
+    },
+    res = 96
+  )
 }
 
 shiny::shinyApp(ui, server)
