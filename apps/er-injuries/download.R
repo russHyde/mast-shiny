a5304bb2a0c0a30ef702ae69ea08aadfaad701c9
Russ Hyde <russ.hyde.data@gmail.com>
2021-03-09 16:28:54 +0000

    explore the ER-injuries dataset

---
+++ apps/er-injuries/download.R
@@ -0,0 +1,13 @@
+dir.create("neiss")
+download <- function(name) {
+  url <- "https://github.com/hadley/mastering-shiny/raw/master/neiss/"
+  download.file(
+    paste0(url, name),
+    paste0("neiss/", name),
+    quiet = TRUE
+  )
+}
+
+download("injuries.tsv.gz")
+download("population.tsv")
+download("products.tsv")
