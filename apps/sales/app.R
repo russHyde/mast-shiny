e1f4441d90465156949eda0024e1eddb081ed86f
Russ Hyde <russ.hyde.data@gmail.com>
2021-03-16 15:29:48 +0000

    fix import of NA==NorthAmerican territory

--- apps/sales/app.R
+++ apps/sales/app.R
@@ -1,9 +1,9 @@
 library(shiny)
 library(readr)
-library(dplyr)
 
 sales <- readr::read_csv(
-  "https://raw.githubusercontent.com/hadley/mastering-shiny/master/sales-dashboard/sales_data_sample.csv"
+  "https://raw.githubusercontent.com/hadley/mastering-shiny/master/sales-dashboard/sales_data_sample.csv",
+  na = ""
 )
 sales <- sales[c(
   "TERRITORY", "ORDERDATE", "ORDERNUMBER", "PRODUCTCODE", "QUANTITYORDERED",
@@ -16,12 +16,10 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  # Note that North America (NA) territory is greyed-out in the printed table,
+  # although it is a valid value
   selected <- reactive({
-    if (input$territory == "NA") {
-      subset(sales, is.na(TERRITORY))
-    } else {
-      subset(sales, TERRITORY == input$territory)
-    }
+    subset(sales, TERRITORY == input$territory)
   })
   output$selected <- renderTable(head(selected(), 10))
 }
